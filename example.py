4f7a34449a7f9f5ff27bd80832a9da6ede7ab0fc
Abhishek <abxisxekpanda@gmail.com>
2022-05-29 16:23:38 +0530

    Search function added and example updated

--- example.py
+++ example.py
@@ -1,6 +1,11 @@
-from urllib import response
 from malwrapper import Anime
 
-anime = Anime(45613) #Anime('id) where id is the anime id which can be obtained from myanimelist.net
+#Example to find an anime using the id
+anime = Anime(id=45613) #Anime('id') where id is the anime id which can be obtained from myanimelist.net
 response = anime.info() # fetching the info function
+print(response)
+
+#Example to find an anime using keywords
+anime = Anime(name='Naruto')
+response = anime.search()
 print(response)
--- malwrapper/anime.py
+++ malwrapper/anime.py
@@ -2,10 +2,16 @@ from . import session
 
 
 class Anime(object):
-    def __init__(self, id):
+    def __init__(self, name=None, id=None):
         self.id = id
+        self.name = name
 
     def info(self):
         path = f'https://api.jikan.moe/v4/anime/{self.id}/full'
         response = session.get(path)
         return response.json()
+
+    def search(self):
+        path = f'https://api.jikan.moe/v4/anime?q={self.name}'
+        search_result = session.get(path)
+        return search_result.json()
